d64711d7c6c6424a04f562ebb554869c0dad9fa1
Carlos Alonso <calonsocrespo@hotmail.com>
2022-07-22 14:31:01 +0200

    add UserRepo and RoleRepo

---
+++ src/main/java/com/cac/portfolio/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package com.cac.portfolio.repo;
+
+import com.cac.portfolio.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
