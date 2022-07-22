318145472444c53ea979506b2e6e95177159c3eb
Carlos Alonso <calonsocrespo@hotmail.com>
2022-07-22 15:29:42 +0200

    add UserService  UserServiceImpl, UserResource and add User and Roles to database

--- src/main/java/com/cac/portfolio/PortfolioApplication.java
+++ src/main/java/com/cac/portfolio/PortfolioApplication.java
@@ -1,7 +1,16 @@
 package com.cac.portfolio;
 
+import com.cac.portfolio.domain.Role;
+import com.cac.portfolio.domain.User;
+import com.cac.portfolio.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -9,5 +18,31 @@ public class PortfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "john","1234",new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will","1234",new ArrayList<>()));
+			userService.saveUser(new User(null, "Jim Carry", "jim","1234",new ArrayList<>()));
+			userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold","1234",new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
 }
+
